8ae120171b3bbe45450010a49cc9cad7a4e070bb
k22rs078 <k22rs078@st.kyusan-u.ac.jp>
2024-04-25 13:55:53 +0900

    Create: SelfIntroduction

---
+++ SelfIntroduction/SelfIntroduction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelfIntroduction
+//
+//  Created by yuuto takeuchi on 2024/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
