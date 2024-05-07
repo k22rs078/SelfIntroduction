bf6259be29e089f3f462a4dd7505783309e2839a
k22rs078 <k22rs078@st.kyusan-u.ac.jp>
2024-05-07 18:48:10 +0900

    update

--- SelfIntroduction/SelfIntroduction/ViewController.swift
+++ SelfIntroduction/SelfIntroduction/ViewController.swift
@@ -8,21 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var helloLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        helloLabel.text = "22RS078"
         
     }
-
-    @IBAction func counterButton(_ sender: UIButton) {
-    }
-    
-    @IBAction func toDoButton(_ sender: UIButton) {
-    }
-    
 }
 
 
