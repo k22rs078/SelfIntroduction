18ca3a63e57228b7f98e1c7b0ed0076430222679
k22rs078 <k22rs078@st.kyusan-u.ac.jp>
2024-05-17 15:20:28 +0900

    update

---
+++ SelfIntroduction/SelfIntroduction/ReversiViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ReversiViewController.swift
+//  SelfIntroduction
+//
+//  Created by yuuto takeuchi on 2024/05/13.
+//
+
+import Foundation
+
+import UIKit
+
+//class ReversiViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+//        return 12
+//    }
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+//        let cell:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: )
+//        let label = cell.contentView.viewWithTag(1) as! UILabel
+//        label.text = String(indexPath.row + 1)
+//        return cell
+//    }
+//}
